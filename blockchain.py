12a4a0954b9539c296f971e2a2686c9b59fdd109
sbs <sbs@asd.com>
2023-12-19 16:36:55 +0900

    change blockchain

--- blockchain.py
+++ blockchain.py
@@ -2,3 +2,17 @@ import datetime
 import hashlib
 import json
 
+class blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': str(datetime.datetime.now()),
+            'proof': proof,
+            'previous_hash': previous_hash
+        }
+        self.chain.append(block)
+        return block
